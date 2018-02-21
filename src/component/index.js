7fccbec97ab7fd72ca53f1804116f0417560c86e
Christofer Evremar <christofer.evremar@chasacademy.se>
2018-02-21 13:13:12 +0100

    let's separate the app into pieces

--- src/component/index.js
+++ src/component/index.js
@@ -1,1 +1,4 @@
-export ...
+export { default as Header } from './Header/';
+export { default as Content } from './Content/Content';
+export { default as Footer } from './Footer/Footer';
+export { default as Form } from './Form/Form';
