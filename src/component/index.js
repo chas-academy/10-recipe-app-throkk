f828fe57ef5abfe9515a492c84fbfb05207397fc
Christofer Evremar <christofer.evremar@chasacademy.se>
2018-02-21 14:05:38 +0100

    oops, I'm using the 'index.js' way of loading the modules, updated

--- src/component/index.js
+++ src/component/index.js
@@ -1,4 +1,4 @@
 export { default as Header } from './Header/';
-export { default as Content } from './Content/Content';
-export { default as Footer } from './Footer/Footer';
-export { default as Form } from './Form/Form';
+export { default as Content } from './Content/';
+export { default as Footer } from './Footer/';
+export { default as Form } from './Form/';
