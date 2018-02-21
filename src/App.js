c1c54435f42b5ea9492d8c27916385d37e3e3343
Christofer Evremar <christofer.evremar@chasacademy.se>
2018-02-21 13:51:22 +0100

    Let's decide whether or not the name of the app is 'Weather or Hot'

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Header, Content, Footer, Form } from './component';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +10,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Weather or Hot</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
